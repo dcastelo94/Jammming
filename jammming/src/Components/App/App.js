f8ce3b79b17b7c76060ccd9537b48a8791472959
dcastelo94 <71777856+dcastelo94@users.noreply.github.com>
2020-10-02 01:18:37 +0100

    App finished but SavePlaylist isn't saving anything to Spotify

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -10,13 +10,9 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-            searchResults: [{name: "name1", artist: "artist1", album: "album1", id: 1}, 
-            {name: "name2", artist: "artist2", album: "album2", id: 2}, 
-            {name: "name3", artist: "artist3", album: "album3", id: 3} ],
+            searchResults: [],
             playlistName: "New Playlist",
-            playlistTracks: [{name: "name1", artist: "artist1", album: "album1", id: 1}, 
-            {name: "name2", artist: "artist2", album: "album2", id: 2}, 
-            {name: "name3", artist: "artist3", album: "album3", id: 3}],
+            playlistTracks: [],
             isRemoval: false
         };
         this.addTrack = this.addTrack.bind(this);
@@ -48,6 +44,12 @@ class App extends React.Component {
 
     savePlaylist() {
         const trackURIs = this.state.playlistTracks.map(track => track.uri);
+        Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
+            this.setState({
+                playlistName: "New Playlist",
+                playlistTracks: []
+            });
+        })
     }
 
     search(term) {
--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -15,7 +15,7 @@ const Spotify = {
         const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
         
         if (accessTokenMatch && expiresInMatch) {
-            const accessToken = accessTokenMatch[1];
+            let accessToken = accessTokenMatch[1];
             const expiresIn = Number(expiresInMatch[1]);
             //set the token to expire and clears the parameters from the URL
             window.setTimeout(() => accessToken = '', expiresIn * 1000);
@@ -46,6 +46,36 @@ const Spotify = {
                 album: track.album.name,
                 uri: track.uri
             }));
+        });
+    },
+
+    savePlaylist(playlistName, URIarray) {
+        if(!playlistName || !URIarray.length) {
+            return;
+        }
+        const accessToken = Spotify.getAccessToken();
+        const headers = { Authorization: `Bearer ${accessToken}` };
+        let userId;
+
+        return fetch("https://api.spotify.com/v1/me", { headers: headers }
+        ).then(response => response.json()
+        ).then(jsonResponse => {
+            userId = jsonResponse.id;
+            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, 
+            { 
+                headers: headers,
+                method: "POST",
+                body: JSON.stringify({ name: playlistName })
+            }).then(response => response.json()
+            ).then(jsonResponse => {
+                const playlistId = jsonResponse.id;
+                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, 
+                {
+                    headers: headers,
+                    method: "POST",
+                    body: JSON.stringify({ uris: URIarray })
+                });
+            });
         });
     }
 }
